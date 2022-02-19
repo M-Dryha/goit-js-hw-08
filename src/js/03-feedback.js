c8cf4977c6a82eb0947716009e1587f6cc964ac5
Mariia Dryha <m.pishchanska@gmail.com>
2022-02-19 09:33:21 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -15,30 +15,28 @@ formElem.addEventListener('input', (e) => {
    JSON.stringify(objectStorage[e.target.name] = e.target.value);
     console.log(objectStorage);
     
-    
+
 })
 
 
-function textAreaInput(event){
-    const message = event.target.value;
-    localStorage.setItem(STORAGE_KEY, message);
-   
+// function textAreaInput(event) {
+//     const message = event.target.value;
+//     localStorage.setItem(STORAGE_KEY, message);
+// }
+
+// function onFormSubmit(event) {
+//     event.preventDefault();
+//     console.log(objectStorage);
+//     event.currentTarget.reset();
+//     localStorage.removeItem(STORAGE_KEY);
     
-}
+// }
 
-function onFormSubmit(event) {
-    event.preventDefault();
-    console.log(objectStorage);
-    event.currentTarget.reset();
-    localStorage.removeItem(STORAGE_KEY);
-    
-}
+// function populateTextArea() {
+//     const textAreaMsg = localStorage.getItem(STORAGE_KEY);
+//     if (textAreaMsg) {
+//         textArea.value = textAreaMsg;
+//         console.log(textAreaMsg);
+//     }
 
-function populateTextArea() {
-    const textAreaMsg = localStorage.getItem(STORAGE_KEY);
-    if (textAreaMsg) {
-        textArea.value = textAreaMsg;
-        console.log(textAreaMsg);
-    }
-
-}
+// }
