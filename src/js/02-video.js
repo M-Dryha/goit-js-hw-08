fd3dad24599cca04012696016340b8d403de8a54
Mariia Dryha <m.pishchanska@gmail.com>
2022-02-20 21:10:05 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,11 +1,11 @@
-
+import throttle from "lodash.throttle";
 
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
 
 const KEY = "videoplayer-current-time";
 const newTime = localStorage.getItem(KEY);
-player.on('timeupdate', onPlay);
+player.on('timeupdate', throttle(onPlay, 1000));
  
 if (newTime) {
     player.setCurrentTime(newTime);
